5c84d6c3a68f4d37ec1464c1feff54d6053c9f8d
Prince mishra <119921857+princemishra9@users.noreply.github.com>
2022-12-30 22:31:16 +0530

    Add files via upload

---
+++ finalmoduletest1-30dec/index.js
@@ -0,0 +1,11 @@
+function submitForm(){
+    var i1 = document.getElementById('firstname');
+    var i2 = document.getElementById('Lastname');
+    var i3 = document.getElementById('Email');
+
+    if (firstname.value === '' | Lastname.value==='' | Email.value===''){
+        
+    }else {
+        document.getElementById('message').style.opacity = '1';
+    }
+}
